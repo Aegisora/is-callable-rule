4ff21d336ec15bc1059af37ef9f86173e676bc54
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-05-11 18:05:20 +0400

    [AEGISORA-9] Covered: context value - empty string.

--- tests/Unit/IsCallableRuleTest.php
+++ tests/Unit/IsCallableRuleTest.php
@@ -92,6 +92,13 @@ class IsCallableRuleTest extends TestCase
                     'failedRuleCode' => 'is_callable_rule',
                 ],
             ],
+            'context value - empty string' => [
+                'context' => Context::create(''),
+                'expectedResult' => [
+                    'isValid' => false,
+                    'failedRuleCode' => 'is_callable_rule',
+                ],
+            ],
         ];
     }
 
