6b141f1403d69f05d29ea08ae67da34e3ff2eddd
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-05-11 17:56:37 +0400

    [AEGISORA-9] Implemented executeValidate.

--- src/IsCallableRule.php
+++ src/IsCallableRule.php
@@ -2,10 +2,21 @@
 
 namespace Aegisora\Rules;
 
-class IsCallableRule
+use Aegisora\RuleContract\Models\Context;
+use Aegisora\RuleContract\Models\Result;
+use Aegisora\RuleContract\Rule;
+
+class IsCallableRule extends Rule
 {
     private static function create(): self
     {
         return new self();
+    }
+
+    protected function executeValidate(Context $context): Result
+    {
+        return is_callable($context->getValue()) ?
+            $this->getDefaultValidResult() :
+            $this->getDefaultInvalidResult();
     }
 }
