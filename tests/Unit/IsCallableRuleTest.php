20892f930dbc00997057281e9cea82107e6d20b6
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-05-11 18:03:46 +0400

    [AEGISORA-9] Covered: context value - negative integer.

--- tests/Unit/IsCallableRuleTest.php
+++ tests/Unit/IsCallableRuleTest.php
@@ -64,6 +64,13 @@ class IsCallableRuleTest extends TestCase
                     'failedRuleCode' => 'is_callable_rule',
                 ],
             ],
+            'context value - negative integer' => [
+                'context' => Context::create(1),
+                'expectedResult' => [
+                    'isValid' => false,
+                    'failedRuleCode' => 'is_callable_rule',
+                ],
+            ],
         ];
     }
 
