7a0701859871136519a640fc3f7c588f39060bd6
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-05-11 18:04:37 +0400

    [AEGISORA-9] Covered: context value - positive float.

--- tests/Unit/IsCallableRuleTest.php
+++ tests/Unit/IsCallableRuleTest.php
@@ -78,6 +78,13 @@ class IsCallableRuleTest extends TestCase
                     'failedRuleCode' => 'is_callable_rule',
                 ],
             ],
+            'context value - positive float' => [
+                'context' => Context::create(0.01),
+                'expectedResult' => [
+                    'isValid' => false,
+                    'failedRuleCode' => 'is_callable_rule',
+                ],
+            ],
         ];
     }
 
